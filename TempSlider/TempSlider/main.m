e0ad0c6bf83deb3100baafdafc775d4d08607514
liqingyang <1402494675@qq.com>
2020-09-15 14:36:46 +0800

    dev_two commit

--- TempSlider/TempSlider/main.m
+++ TempSlider/TempSlider/main.m
@@ -8,9 +8,11 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
-
+// dev_two
 int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
+// dev_two_tip1
+// dev_two_tip2
